b5407d4893ac1ad2eb3dd5c6df637fccbd30ba4f
sazzad <sazzad@ipvision-inc.com>
2015-04-07 12:07:20 +0600

    python start

---
+++ src/Main.py
@@ -0,0 +1,37 @@
+def numbers(*args):
+    total = 0;
+
+    for a in args:
+        total += a
+    print(total)
+
+
+numbers(10, 20)
+
+'''
+__author__ = 'lynas'
+
+
+def m1():
+    print("hello world")
+
+
+m1()
+
+
+def add2number(output='output : ', num1=1, num2=3):
+    return output, num1 + num2
+
+
+print add2number(num1=10, num2=20)
+
+print "hello world"
+print("test")
+
+
+test = [1,5,9,12]
+for n in range(1, 20):
+    if n in test:
+        continue
+    print(n)
+'''
