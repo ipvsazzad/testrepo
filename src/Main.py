a1f9cd9bff5933d12fcff9a81ebdc9d995777f7a
sazzad <sazzad@ipvision-inc.com>
2015-04-07 17:49:24 +0600

    list sort or min max

--- src/Main.py
+++ src/Main.py
@@ -1,2 +1,24 @@
-test = lambda x: x*9
-print(test(99))
+sto = {
+    'google': 123,
+    'fb': 188,
+    'ti': 259,
+    'yahoo': 99
+}
+
+print(
+    min(
+        zip(
+            sto.values(),
+            sto.keys()
+        )
+    )
+)
+
+print(
+    sorted(
+        zip(
+            sto.values(),
+            sto.keys()
+        )
+    )
+)
