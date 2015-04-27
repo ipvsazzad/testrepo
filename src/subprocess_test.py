119855108a6054b970bbfbc9d16145f99c6bc023
sazzad <sazzad@ipvision-inc.com>
2015-04-27 13:02:15 +0600

    json object manupulation for curl output swift result

--- src/subprocess_test.py
+++ src/subprocess_test.py
@@ -12,7 +12,24 @@ proc = subprocess.Popen(
     ],
     stdout=subprocess.PIPE)
 output = proc.stdout.read()
-print "someoutput", output
-#json_obj = json.loads(output)
+json_obj = json.loads(output)
+print "you have " + str(len(json_obj)) + " container(s)"
+for obj in json_obj:
+    print "Container " + str(obj['name']) + " has " + str(obj['count']) + " file(s) "
 
-#print(json_obj)
+for obj in json_obj:
+    print "Container " + str(obj['name'])
+    proc2 = subprocess.Popen(
+        [
+            "curl",
+            "-X",
+            "GET",
+            "-H",
+            "X-Auth-Token: AUTH_tk8bf07349d36041339450f0b46a2adc39",
+            "http://localhost:8080/v1/AUTH_myaccount/"+str(obj['name'])+"?format=json"
+        ],
+        stdout=subprocess.PIPE)
+    output = proc2.stdout.read()
+    json_obj2 = json.loads(output)
+    for obj2 in json_obj2:
+        print obj2['name']
