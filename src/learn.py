5a82dd67be7540bd11401bf424697c09538daba6
sazzad <sazzad@ipvision-inc.com>
2015-04-29 14:05:21 +0600

    learning module started

---
+++ src/learn.py
@@ -0,0 +1,26 @@
+# # if else
+# var1 = 2
+#
+# if var1 > 5:
+# print "yes"
+# elif var1 < 5:
+#     print "no"
+# else:
+#     print "other"
+#
+# # loop
+#
+# var2 = 1
+#
+# while var2 <= 10:
+#     if var2 == 5:
+#         print "got 5"
+#     print var2
+#     var2 += 1
+
+for count in range(1, 11):
+    print (count)
+
+
+
+
