e15f209664b8a5786a7fedfd2b51525727d4e6b7
sazzad <sazzad@ipvision-inc.com>
2015-04-29 14:46:43 +0600

    printing return code for subprocess to check error

--- src/sgc.py
+++ src/sgc.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.call(["git", "add", "--all"])
-subprocess.call(["git", "commit", "-m", raw_input('Enter a commit message: ')])
-subprocess.call(["git", "push"])
+print subprocess.call(["git", "add", "--all"])
+print subprocess.call(["git", "commit", "-m", raw_input('Enter a commit message: ')])
+print subprocess.call(["git", "push"])
 
